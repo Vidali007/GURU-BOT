949d285f1b862aed6cb9d8d97df976f87ec18f4f
Vidali007 <168802688+Vidali007@users.noreply.github.com>
2025-12-23 08:07:38 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const ownervb = process.env.OWNERS || "917605902011;GURU;6281255369012;Ash"
+const ownervb = process.env.OWNERS || "+2348154207037;GURU;6281255369012;Ash"
 
 const ownerlist = ownervb.split(';');
 
@@ -20,13 +20,13 @@ for (let i = 0; i < ownerlist.length; i += 2) {
     global.owner.push(owner);
 }
 
-global.mods = ['917605902011', '32460248586']
-global.allowed = ['917605902011', '32460248586', '19152999993']
+global.mods = ['+2348154207037', '32460248586']
+global.allowed = ['917605902011', '32460248586', '']
 
 // Sticker WM
 global.botname = process.env.BOTNAME
-global.packname = 'GURU┃ᴮᴼᵀ'
-global.author = 'ᴳᵘʳᵘ ˢᵉⁿˢᵉⁱ'
+global.packname = 'SECURED FUTUREᴮᴼᵀ'
+global.author = 'FUTUREˢᵉⁿˢᵉⁱ'
 global.thumb = fs.readFileSync('./Assets/Gurulogo.jpg')
 
 // Status indicators
